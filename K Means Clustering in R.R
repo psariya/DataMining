f6b8ae16dcc3209a32034d11504549e5788893a6
Priyanka Sariya <45498417+psariya@users.noreply.github.com>
2020-05-25 00:55:42 -0600

    Add files via upload

---
+++ K Means Clustering in R.R
@@ -0,0 +1,41 @@
+#install.packages('corrplot')
+library(corrplot)
+# Clustering
+library(cluster) 
+
+data <- read.csv('C:\\Users\\Priyanka\\Documents\\0. USF\\001. USF Volunteer\\Week 4\\Wholesale customers data.csv', header = TRUE)
+head(data,5)
+
+data <- na.omit(data)
+str(data)
+
+summary(data)
+
+
+corrmatrix <- cor(data)
+corrplot(corrmatrix, method = 'number')
+
+df <- data[-c(1,2)]
+head(df,3)
+
+silhouette_score <- function(k){
+  km <- kmeans(df, centers = k, nstart=25)
+  ss <- silhouette(km$cluster, dist(df))
+  mean(ss[, 3])
+}
+k <- 2:10
+avg_sil <- sapply(k, silhouette_score)
+plot(k, type='b', avg_sil, xlab='Number of clusters', ylab='Average Silhouette Scores', frame=FALSE)
+
+km.final <- kmeans(df, 5)
+## Total Within cluster sum of square
+km.final$tot.withinss
+
+## Cluster sizes
+km.final$size
+
+data$cluster <- km.final$cluster
+head(data, 6)
+
+clusplot(data, data$cluster, color=TRUE, shade = TRUE, label=2)
+  
